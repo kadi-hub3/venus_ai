09e3be5d1d0d10bec05a56a01071926b3bff5fcb
kadi-hub3 <kicktheball1@gmail.com>
2024-12-04 20:15:04 +0100

    Clerk Authentication

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard(protected)</div>
+    <div>
+        Dashboard(protected)
+        <UserButton />
+    </div>
   )
 }
 
